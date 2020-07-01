3c20d330383fa19f947204f4a432b43bda3a6175
ian-code <56250354+ian-code@users.noreply.github.com>
2020-07-01 13:15:40 +0300

    Create demo.java

---
+++ demo.java
@@ -0,0 +1,25 @@
+package com.trial;
+
+import java.io.*;
+import java.util.*;
+public class Circle {
+
+    public static void main(String[] args)
+    throws FileNotFoundException{
+
+     Map<Integer,String> student=new HashMap<>();
+
+        student.put(1,"dave");
+        student.put(2,"den");
+        student.put(3,"sally");
+        student.put(4,"house");
+        student.put(5,"kelly");
+
+System.out.println(student.get(2));
+
+     student.keySet();
+        System.out.println(student.entrySet());
+        System.out.println(student.values());
+}
+
+}
